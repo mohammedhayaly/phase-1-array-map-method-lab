5a4ef7d2a4019456c6bb946924c3f12ae62be86d
mohammedhayaly <mohammed.alhayali88@yahoo.com>
2023-07-20 11:47:49 +0300

    done

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
-];
+const { expect } = require('chai');
+const { titleCased } = require('../index');
 
-const titleCased = () => {
-  return tutorials
-}
+describe('index.js', function () {
+  describe('titleCased()', function () {
+    it('returns an array with title case tutorial names', function () {
+      const result = titleCased();
+      const expectedResult = [
+        'What Does The This Keyword Mean?',
+        'What Is The Constructor OO Pattern?',
+        'Implementing Blockchain Web API',
+        'The Test Driven Development Workflow',
+        'What Is NaN And How Can We Check For It',
+        'What Is The Difference Between StopPropagation And PreventDefault?',
+        'Immutable State And Pure Functions',
+        'What Is The Difference Between == And ===?',
+        'What Is The Difference Between Event Capturing And Bubbling?',
+        'What Is JSONP?',
+      ];
+      expect(result).to.eql(expectedResult);
+    });
+  });
+});
--- test/indexTest.js
+++ test/indexTest.js
@@ -1,22 +1,23 @@
-require ( './helpers.js' );
+const tutorials = [
+  'what does the this keyword mean?',
+  'What is the Constructor OO pattern?',
+  'implementing Blockchain Web API',
+  'The Test Driven Development Workflow',
+  'What is NaN and how Can we Check for it',
+  'What is the difference between stopPropagation and preventDefault?',
+  'Immutable State and Pure Functions',
+  'what is the difference between == and ===?',
+  'what is the difference between event capturing and bubbling?',
+  'what is JSONP?',
+];
 
-describe('index.js', function () {
-  describe('titleCased()', function () {
-    it('returns an array with title case tutorial names', function () {
-      expect(titleCased()).to.have.all.members(
-        [
-          "What Does The This Keyword Mean?",
-          "What Is The Constructor OO Pattern?",
-          "Implementing Blockchain Web API",
-          "The Test Driven Development Workflow",
-          "What Is NaN And How Can We Check For It",
-          "What Is The Difference Between StopPropagation And PreventDefault?",
-          "Immutable State And Pure Functions",
-          "What Is The Difference Between == And ===?",
-          "What Is The Difference Between Event Capturing And Bubbling?",
-          "What Is JSONP?"
-      ]
-      )
-    });
+function titleCased() {
+  return tutorials.map((tutorial) => {
+    return tutorial
+      .split(' ')
+      .map((word) => word[0].toUpperCase() + word.slice(1))
+      .join(' ');
   });
-});
+}
+
+module.exports = { titleCased };
